b26e0733e3c2a82f24d97b7f166cd5eb169af530
scottmarler <scott.marler@gmail.com>
2017-10-17 16:12:27 -0700

    Part of an MPV!

--- bulk_processing.py
+++ bulk_processing.py
@@ -8,9 +8,10 @@ import urllib
 import zipfile
 from collections import defaultdict
 import requests
+from keys import Keys
 
-propublica_api_key = "ODFvfKwCNwTHCbQQf4a3fmCkdW0qU0GnKKpHCe8q"
-google_api_key = "AIzaSyAMNlvrMsN-mlVz3_u2bPoaWNG_XhzAs-Y"
+propublica_api_key = Keys.ProPublica()
+google_api_key = Keys.Google()
 
 ###############################################################################
 #
@@ -147,13 +148,10 @@ def score_bill(b):
             return status_scores['reported']
         elif "REFERRED" in status:
             return status_scores['referred']
-        
-        #elif "PASSED:CONCURRENTRES" in status:
-            # Concurrent resolution passed (cannot become law)
-        #    return 3
-        #elif "PASSED:SIMPLERES" in status:
-            # Simple resolution passed (cannot become law, only requires one chamber to approve)
-        #    return 2
+        elif "PASSED:CONCURRENTRES" in status:
+            return status_scores['passed_conres']
+        elif "PASSED:SIMPLERES" in status:
+            return status_scores['passed_simpleres']
         
         else:
             pass
@@ -204,6 +202,60 @@ def aggregate_scores_by_person(person):
     else:
         person['cosponsor_mean'] = 0.0
     person['cosponsor_status_counts'] = {x: len([i for i in person['cosponsor_scores'] if i == status_scores[x]]) for x in status_scores}
+
+def ppp(bill, bipart_threshold=.25):
+    '''
+    The Partisanship Pants Party helper function!
+    
+    Populates the partisanship dictionary, part_dict
+    
+    Brick Tamland: Excuse me, Veronica? 
+    Veronica Corningstone: Yes? What is it, Brick? 
+    Brick Tamland: I would like to extend to you an invitation to the pants party. 
+    Veronica Corningstone: Excuse me? 
+    Brick Tamland: [struggling] The... party. With the... with the pants. Party with pants? 
+    Veronica Corningstone: Brick, are you saying that there's a party in your pants and that I'm invited? 
+    Brick Tamland: That's it. 
+    Veronica Corningstone: Did Brian tell you to say this, Brick? 
+    Brick Tamland: No. Yes. He did. 
+    Veronica Corningstone: Okay. No. I don't want to go to a party in your pants. 
+    Brick Tamland: Very well. Ian, would you like to go to a party in my pants? 
+    Ian: No, Brick. 
+    Brick Tamland: All right. Let's go. 
+    '''
+    sponsor_id = bill['sponsor']['bioguide_id']
+    sponsor_party = party_lookup[sponsor_id]
+    current_sponsor = sponsor_id in part_dict 
+        # as in current member of congress
+    co_same = 0
+    co_diff = 0
+    
+    for csp in bill['cosponsors']:
+        cosponsor_id = csp['bioguide_id']
+        cosponsor_party = party_lookup[cosponsor_id]
+        current_cosponsor = cosponsor_id in part_dict
+            # as in current member of congress
+        if current_cosponsor:
+            part_dict[cosponsor_id]['as_cosponsor']['count'] += 1
+        if (cosponsor_party in left and sponsor_party in left) or (cosponsor_party in right and sponsor_party in right):
+            co_same += 1
+            if current_cosponsor:
+                part_dict[cosponsor_id]['as_cosponsor']['sponsor_same'] += 1
+            if current_sponsor:
+                part_dict[sponsor_id]['as_sponsor']['cosponsor_same'] += 1
+        else:
+            co_diff += 1
+            if current_cosponsor:
+                part_dict[cosponsor_id]['as_cosponsor']['sponsor_different'] += 1
+            if current_sponsor:
+                part_dict[sponsor_id]['as_sponsor']['cosponsor_different'] += 1
+    
+    if current_sponsor:
+        part_dict[sponsor_id]['as_sponsor']['count'] += 1
+        if len(bill['cosponsors']) > 0:
+            if co_diff * 1.0 / (co_diff + co_same) > bipart_threshold:
+                part_dict[sponsor_id]['as_sponsor']['bipartisan_support'] += 1
+    
     
 ###############################################################################
 #
@@ -214,7 +266,7 @@ def aggregate_scores_by_person(person):
 folderstring = './Data/ProPublicaBulk'
 
 # Ensure this function call is not commented out if you want updates to happen
-#bulk_update()
+bulk_update()
 
 for cngrss in ['115','114','113']:
 
@@ -225,7 +277,9 @@ for cngrss in ['115','114','113']:
     # Build dictionaries for all current senators and reps
     senators = {}
     representatives = {}
-    relevant_keys = ["first_name", "last_name", "party", "state", "total_votes", "votes_with_party_pct"]
+    relevant_keys = ["first_name", "last_name", "party", "state", 
+                     "total_votes", "votes_with_party_pct", "dw_nominate",
+                     "missed_votes_pct", "seniority"]
 
     for member in s_data:
         senators[member["id"]] = {}
@@ -249,8 +303,8 @@ for cngrss in ['115','114','113']:
 
     #Only uncomment one of the following 2 lines:
 
-    #bill_types = os.listdir("./data/ProPublicaBulk/115/bills") # This line includes all bill types
-    bill_types = ['hjres', 'hr', 's', 'sjres'] # This line restricts bill types to those that can become law
+    bill_types = os.listdir("./data/ProPublicaBulk/115/bills") # This line includes all bill types
+    #bill_types = ['hjres', 'hr', 's', 'sjres'] # This line restricts bill types to those that can become law
 
     #This dict defines how much each "farthest reached status" is worth for scoring
     status_scores = {'enacted':10,    # Bill becomes law
@@ -258,7 +312,9 @@ for cngrss in ['115','114','113']:
                      'pass_back':5,   # The bill has passed each chamber at least once
                      'pass_over':4,   # The bill has passed at least one chamber
                      'reported':2,    # The bill has been "reported out of committee"
-                     'referred':1}    # At least they tried to do *something*, but didn't make it out of committee
+                     'referred':1,    # At least they tried to do *something*, but didn't make it out of committee
+                     'passed_conres':3.5,
+                     'passed_simpleres':2.5}    
                  
 
     # Define and populate dictionaries for bills in each congress
@@ -276,6 +332,49 @@ for cngrss in ['115','114','113']:
         representatives[r]['bill_scores'] = []
         representatives[r]['sponsor_scores'] = []
         representatives[r]['cosponsor_scores'] = []
+    
+    #Gonna need this later...
+    party_lookup = {}
+    for c in ['113','114','115']:
+        for member in all_senators(congress=c)["results"][0]["members"]:
+            party_lookup[member['id']] = member['party']
+        for member in all_reps(congress=c)["results"][0]["members"]:
+            party_lookup[member['id']] = member['party']
+    
+    # As of right now, there are only Democrats, Republicans, and Independents in Congress
+    # None in the House, 2 in the Senate
+    # Both Independents in the Senate caucus with the Democrats
+    # So for this purpose, they are considered to be the "same"
+    left = ['D','I']
+    right = ['R']
+
+    part_dict = {}
+
+    for s in senators.keys():
+        part_dict[s] = {'as_sponsor': {'count': 0,
+                                       'cosponsor_same': 0,
+                                       'cosponsor_different': 0,
+                                       'bipartisan_support': 0},
+                        'as_cosponsor': {'count': 0,
+                                         'sponsor_same': 0,
+                                         'sponsor_different': 0},
+                        'chamber': 'Senate',
+                        'party': senators[s]['party'],
+                        'name': senators[s]['first_name'] + ' ' + senators[s]['last_name']
+                        }
+        
+    for r in representatives.keys():
+        part_dict[r] = {'as_sponsor': {'count': 0,
+                                       'cosponsor_same': 0,
+                                       'cosponsor_different': 0,
+                                       'bipartisan_support': 0},
+                        'as_cosponsor': {'count': 0,
+                                         'sponsor_same': 0,
+                                         'sponsor_different': 0},
+                        'chamber': 'House',
+                        'party': representatives[r]['party'],
+                        'name': representatives[r]['first_name'] + ' ' + representatives[r]['last_name']
+                        }   
         
     # Loop through every bill in each congress
     # For each bill, build a list of statuses, in order
@@ -295,6 +394,9 @@ for cngrss in ['115','114','113']:
                 c[bt][b]['score'] = score_bill(c[bt][b])
                 
                 distribute_scores(c[bt][b], bt[0])
+                
+                # call the helper function created above to populate the partisanship dictionary
+                ppp(c[bt][b])
                 
     for s in senators:
         aggregate_scores_by_person(senators[s])
@@ -381,16 +483,33 @@ for cngrss in ['115','114','113']:
             representatives[r]['cosponsor_normalized'] = co_mean / hdc_max
             representatives[r]['cosponsor_rank'] = len([i for i in hdc_scores if i > co_mean]) + 1
             
+    # looping again, this time to fold in the partisanship figures
+    
+    for s in senators:
+        senators[s]['bi_sponsor_count'] = part_dict[s]['as_sponsor']['count']
+        senators[s]['bi_sponsor_bi'] = part_dict[s]['as_sponsor']['bipartisan_support']
+        
+    for r in representatives:
+        representatives[r]['bi_sponsor_count'] = part_dict[r]['as_sponsor']['count']
+        representatives[r]['bi_sponsor_bi'] = part_dict[r]['as_sponsor']['bipartisan_support']
+    
     # prep to build pandas dataframe for output file
 
     fields = ['party', 
               'state', 
+              'total_votes',
+              'votes_with_party_pct',
+              'dw_nominate',
+              'missed_votes_pct',
+              'seniority',
               'cosponsor_count', 
               'cosponsor_normalized', 
               'cosponsor_rank', 
               'sponsor_count',
               'sponsor_normalized',
-              'sponsor_rank']
+              'sponsor_rank',
+              'bi_sponsor_count',
+              'bi_sponsor_bi']
 
     senate_rows_list = []
     house_rows_list = []
@@ -412,6 +531,25 @@ for cngrss in ['115','114','113']:
 
     senators_df['color'] = senators_df['party'].map(lambda x: {'R': 'red', 'D': 'blue', 'I': 'blue'}[x])
     representatives_df['color'] = representatives_df['party'].map(lambda x: {'R': 'red', 'D': 'blue', 'I': 'blue'}[x])
+    
+    senators_df['size'] = 20
+    representatives_df['size'] = 20
+    
+    senators_df['bi_pct'] = senators_df.bi_sponsor_bi * 1.0 / senators_df.bi_sponsor_count
+    representatives_df['bi_pct'] = representatives_df.bi_sponsor_bi * 1.0 / representatives_df.bi_sponsor_count
+    
+    senators_df['chamber'] = 'senate'
+    representatives_df['chamber'] = 'house'
+    
+    senators_df['congress'] = cngrss
+    representatives_df['congress'] = cngrss
 
-    senators_df.to_csv('%s_senator_effectiveness.csv' %cngrss, encoding='utf-8')
-    representatives_df.to_csv('%s_house_effectiveness.csv' %cngrss, encoding='utf-8')
+    if 'final_out_df' in locals():
+        final_out_df = pd.concat([final_out_df, senators_df, representatives_df])
+    else:
+        final_out_df = pd.concat([senators_df, representatives_df])
+    
+    #senators_df.to_csv('%s_senator_effectiveness.csv' %cngrss, encoding='utf-8')
+    #representatives_df.to_csv('%s_house_effectiveness.csv' %cngrss, encoding='utf-8')
+
+final_out_df.to_csv('effectiveness.csv', encoding='utf-8')
