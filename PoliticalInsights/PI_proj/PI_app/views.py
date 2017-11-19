bde57c5b2b1fa3d76856866efd8f54e168c1d315
Lisa Minas <lisa.minas@gmail.com>
2017-11-19 13:23:40 -0800

    Address field form sending information

--- PoliticalInsights/PI_proj/PI_app/views.py
+++ PoliticalInsights/PI_proj/PI_app/views.py
@@ -6,39 +6,69 @@ from __future__ import unicode_literals
 
 # django specific imports
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # model imports
 from .models import Effectiveness
+from .forms import UserAddress, SelectRep, SelectMetricType
 import api_utils
 
 def home(request):
-    
-    # identify user address
-    address = "1313 Disneyland Dr, Anaheim, CA 92802" #TODO - get user input as address
 
-    # retrieve representatives for given address
-    # google response is in form:
-    #   {chamber:
-    #      {rep_id:
-    #         {image_url: 'https://...',
-    #          name: 'Joe Dirt'
-    #         }
-    #      }
-    #   }
-    g = api_utils.Google()
-    my_reps_g = g.ids_from_address(address)
+    # setup default values for return statement
+    # returns these values if GET (or any other method)
     my_reps = {}
-    for chamber, member_list in my_reps_g.items():
-        for member_id, member_info in member_list.items():
-            my_reps[member_id] = {'chamber': chamber,
-                                  'img_url': member_info['img_url'],
-                                  'name': member_info['name']
-                                  }
-    #        print "chamber: {}".format(member_info['name'])
-    #        display(Image(url=member_info['image_url'], height=165, width=135))
+    user_address_form = UserAddress()
+    selected_rep_form = SelectRep()
+    selected_metric_form = SelectMetricType()
+    all_reps_effectiveness = Effectiveness.objects.all() #TODO may not be needed?
+
+    
+    # if POST request (user entered information) then process form data
+    if request.method == 'POST':
+        user_address = request.POST['user_address']
+        
+        # user address in POST      # TODO-make this into the address widget
+        if user_address != '':
+            user_address_form = UserAddress(request.POST)
             
-    all_reps_effectiveness = Effectiveness.objects.all()
+            # retrieve representatives for given address
+            # google response is in form:
+            #   {chamber:
+            #      {rep_id:
+            #         {image_url: 'https://...',
+            #          name: 'Joe Dirt'
+            #         }
+            #      }
+            #   }
+            g = api_utils.Google()
+            my_reps_g = g.ids_from_address(user_address)
+            
+            for chamber, member_list in my_reps_g.items():
+                for member_id, member_info in member_list.items():
+                    my_reps[member_id] = {'chamber': chamber,
+                                          'img_url': member_info['img_url'],
+                                          'name': member_info['name']
+                                          }
+
+            # reset SelectRep and SelectMetric
+            selected_rep_form = SelectRep()
+            selected_metric_form = SelectMetricType()
+
+        # rep selected in POST
+        elif 'selected_rep' in request.POST:
+            selected_rep_form = SelectRep(request.POST)
+
+        # metric selected in POST
+        elif 'selected_metric' in request.POST:
+            selected_metric_form = SelectMetricType(request.POST)
+
+    
     return render(request,
                   'home.html',
-                  {'effectiveness': all_reps_effectiveness, 'my_reps': my_reps}
-                  )
+                  {'my_reps': my_reps,
+                   'user_address_form': user_address_form,
+                   'selected_rep_form': selected_rep_form,
+                   'selected_metric_form': selected_metric_form
+                  },
+                 )
